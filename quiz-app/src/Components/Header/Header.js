ed2a459d7b8cd8072a29c39045ef0a4e25eaddb4
ShakyaSaurav <sauravshakya2015@gmail.com>
2023-01-03 13:22:19 +0545

    legacy version matching

--- quiz-app/src/Components/Header/Header.js
+++ quiz-app/src/Components/Header/Header.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import {link} from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
-  return (
-    <BrowserRouter>
-    <div className='header'>
-      <link to='/' className='title'>
-      Intuituve Quiz Hub
-      </link>  
-      <hr className='divider'/>
-    </div>
-    </BrowserRouter>
+  return(
+  <div>
+    <Link>
+    </Link>
+  </div>
   )
 }
 
