fd51a5ec27a6df8d9bf431202668f6e4a6bc92c7
ShakyaSaurav <sauravshakya2015@gmail.com>
2023-01-04 15:46:50 +0545

    ui front almost done

--- quiz-app/src/Pages/Home/Home.js
+++ quiz-app/src/Pages/Home/Home.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import "./Home.css"
-import {TextField} from '@material-ui/core';
-const Home = () => {
+import {Button, colors, MenuItem, TextField} from '@material-ui/core';
+import Categories from '../../Data/Categories';
+import { useState } from 'react';
+
+const Home = ({name,setName}) => {
+  const [category, setCategory] = useState('');
+  const [difficulty, setdifficulty] = useState('');
+ 
   return (
     <div className='content'>
       <div className='settings'>
@@ -10,7 +16,45 @@ const Home = () => {
         <div className='settings_select'>
           <TextField style={{marginBottom:25}} 
           label='Enter Your Name' 
-          variant='outlined'/>
+          variant='outlined'
+          onChange={(e)=> setName(e.target.value)}
+          
+          />
+          <TextField select
+          label='Select Category' 
+          variant='outlined'
+          style={{marginBottom:30}}
+          onChange={(e)=>setCategory(e.target.value)}
+          value={category}
+          >
+            {Categories.map((cat)=>(
+              <MenuItem key={cat.category} value={cat.value}>
+                {cat.category}
+              </MenuItem>
+            ))}
+              
+            </TextField>
+            <TextField select
+          label='Select Difficulty' 
+          variant='outlined'
+          style={{marginBottom:30}}
+          onChange={(e)=> setdifficulty(e.target.value)}
+          value={difficulty}
+          >
+              <MenuItem key="Easy" value='easy'>
+                Easy
+              </MenuItem>
+              <MenuItem key="Medium" value='medium'>
+                Medium
+              </MenuItem>
+              <MenuItem key="Hard" value='hard'>
+                Hard
+              </MenuItem>
+            </TextField>
+            <Button variant='contained' color='primary' size='large'>
+              Start Quiz
+            </Button>
+
         </div>
       
       
