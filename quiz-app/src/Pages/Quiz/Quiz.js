05c864bd8e3ee01eb29fb3bb4d24825b8dc55ac2
ShakyaSaurav <sauravshakya2015@gmail.com>
2023-01-05 17:03:46 +0545

    retrieving questions annd shuffle options. now onto the quiz ui

--- quiz-app/src/Pages/Quiz/Quiz.js
+++ quiz-app/src/Pages/Quiz/Quiz.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 const Quiz = ({name,score,questions,setQuestions,setScore}) => {
-  return (
-    <div>Quiz</div>
-  )
-}
+  const [options, setOptions] = useState();
+  const [currQues, setCurrQues] = useState(0);
+  useEffect(()=>{
+    console.log(questions);
 
-export default Quiz
+    setOptions(questions && handleShuffle([questions[currQues]?.correct_answer,
+    ...questions[currQues]?.incorrect_answers,])
+    );
+  },[questions]);
+
+  console.log(options);
+  
+  const handleShuffle=(optionss)=>{
+
+  return optionss
+  .sort(()=>Math.random() - 0.5);
+  }
+  
+  return (
+    <div>
+      <span className='subtitle'> Welcome,{name}
+      </span>
+      </div>
+  )
+};
+
+export default Quiz;
