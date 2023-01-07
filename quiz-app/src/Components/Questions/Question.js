270a8324c383ef51d9867b05622953ef2655c68e
ShakyaSaurav <sauravshakya2015@gmail.com>
2023-01-07 18:25:29 +0545

    handling option select.

--- quiz-app/src/Components/Questions/Question.js
+++ quiz-app/src/Components/Questions/Question.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import { useState } from 'react'
+import ErrorMessage from '../ErrorMessage/ErrorMessage'
+import './Question.css'
 
 const Question = ({
   currQues,
@@ -11,14 +14,46 @@ const Question = ({
   setQuestions,
 
 }) => {
-  const [selected, setSelected] = uSeState()
+  const [selected, setSelected] = useState()
   const [error, setError] = useState(false)
+
+  const handleSelect=(i)=>{
+    if(selected===i && selected===correct){
+      return 'select';
+    }
+    else if(selected===i && selected!== correct){
+      return 'wrong';
+    }
+    else if(i===correct){
+      return 'select';
+    }
+  };
+  const handleCheck=(i)=>{
+    setSelected(i);
+    if(i=== correct) setScore(score+1);
+    setError(false);
+  }
+  
   return (
-    <div>
+    <div className='question'>
       <h1>Question {currQues + 1}</h1>
       <div className='singleQuestion'>
         <h2>{questions[currQues].question}</h2>
-        <div className='options'></div>
+        <div className='options'>
+          {error && <ErrorMessage>{error}</ErrorMessage>}
+          {
+            options && options.map(i=>(
+              <button onClick={()=>handleCheck(i)
+              }
+              className={`singleOption ${selected && handleSelect(i)}`}
+              key={i}
+              disabled={selected}
+              >
+                {i}
+              </button>
+            ))
+          }
+        </div>
       </div>
     </div>
   )
